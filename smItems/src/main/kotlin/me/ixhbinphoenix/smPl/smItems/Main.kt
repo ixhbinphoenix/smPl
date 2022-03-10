a3a061f672cf0aae9b83278f5e2ff8da632280ef
renkertm <58270935+renkertm@users.noreply.github.com>
2022-03-10 21:47:23 +0100

    Suppressed Unused Warning

--- smItems/src/main/kotlin/me/ixhbinphoenix/smPl/smItems/Main.kt
+++ smItems/src/main/kotlin/me/ixhbinphoenix/smPl/smItems/Main.kt
@@ -2,6 +2,7 @@ package me.ixhbinphoenix.smPl.smItems
 
 import org.bukkit.plugin.java.JavaPlugin
 
+@Suppress("unused")
 class Main : JavaPlugin() {
     override fun onEnable() {
         server.consoleSender.sendMessage("§asmItems enabled")

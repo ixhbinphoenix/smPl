a6ad2a4ee9b6477e7c25f425859a170228e0b060
renkertm <58270935+renkertm@users.noreply.github.com>
2022-03-22 18:23:47 +0100

    fix(both): fixed stats calculation #3

--- smItems/src/main/kotlin/me/ixhbinphoenix/smPl/smItems/commands/Commands.kt
+++ smItems/src/main/kotlin/me/ixhbinphoenix/smPl/smItems/commands/Commands.kt
@@ -37,33 +37,39 @@ class Commands : CommandExecutor,Listener {
                 .append(player.name().color(NamedTextColor.RED))
                 .append(Component.text(
                   "\nDamage: ${playerHandler.getDamage()}" +
-                          "\nMana: ${playerHandler.getMana()}" +
-                          "\n"
+                          "\nMana: ${playerHandler.getMana()}"
                 ).color(NamedTextColor.GOLD))
               player.sendMessage(message)
             }
-          }
-          val item = sender.inventory.getItem(sender.inventory.heldItemSlot) as ItemStack
-          if (item.hasItemMeta()) {
-            val im = item.itemMeta as ItemMeta
-            var msg = Component.text("psdc info for ").color(NamedTextColor.GOLD)
-              .append(im.displayName()!!.color(NamedTextColor.RED))
-            for (key in im.persistentDataContainer.keys) {
-              var keyVal: Any = -2
-              when {
-                key.toString().endsWith("int") -> {
-                  keyVal = im.persistentDataContainer.getOrDefault(key, PersistentDataType.INTEGER, -1)
-                }
-                key.toString().endsWith("str") -> {
-                  keyVal = im.persistentDataContainer.getOrDefault(key, PersistentDataType.STRING, "-1")
-                }
-              }
-              msg = msg.append(Component.text("\n$key = $keyVal").color(NamedTextColor.GOLD))
+            if(args[0] == "reset"){
+              val playerHandler = PlayerHandler(sender)
+              playerHandler.setDamage(0)
+              playerHandler.setMana(0)
             }
-            sender.sendMessage(msg)
-          } else {
-            val msg = Component.text("Item " + item.type + " does not have any item meta").color(NamedTextColor.RED)
-            sender.sendMessage(msg)
+          }
+          else{
+            val item = sender.inventory.getItem(sender.inventory.heldItemSlot) as ItemStack
+            if (item.hasItemMeta()) {
+              val im = item.itemMeta as ItemMeta
+              var msg = Component.text("psdc info for ").color(NamedTextColor.GOLD)
+                .append(im.displayName()!!.color(NamedTextColor.RED))
+              for (key in im.persistentDataContainer.keys) {
+                var keyVal: Any = -2
+                when {
+                  key.toString().endsWith("int") -> {
+                    keyVal = im.persistentDataContainer.getOrDefault(key, PersistentDataType.INTEGER, -1)
+                  }
+                  key.toString().endsWith("str") -> {
+                    keyVal = im.persistentDataContainer.getOrDefault(key, PersistentDataType.STRING, "-1")
+                  }
+                }
+                msg = msg.append(Component.text("\n$key = $keyVal").color(NamedTextColor.GOLD))
+              }
+              sender.sendMessage(msg)
+            } else {
+              val msg = Component.text("Item " + item.type + " does not have any item meta").color(NamedTextColor.RED)
+              sender.sendMessage(msg)
+            }
           }
         }
         command.name.lowercase() == "customitem" -> {
--- smItems/src/main/kotlin/me/ixhbinphoenix/smPl/smItems/events/Events.kt
+++ smItems/src/main/kotlin/me/ixhbinphoenix/smPl/smItems/events/Events.kt
@@ -1,7 +1,11 @@
 package me.ixhbinphoenix.smPl.smItems.events
 
+import me.ixhbinphoenix.smPl.smItems.Main
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerItemHeldEvent
 
@@ -18,6 +22,17 @@ class Events : Listener {
     }
     else if(newItem!!.hasItemMeta() && oldItem!!.hasItemMeta()){
       StatsCalculation().changeStats(oldItem.itemMeta, newItem.itemMeta, event.player)
+    }
+  }
+
+  @EventHandler
+  fun onInventoryClick(event: InventoryClickEvent){
+    if(event.inventory.viewers.size >= 2) return
+    val player = event.whoClicked as Player
+    val slot = event.slot
+    val item = event.currentItem
+    if(slot == player.inventory.heldItemSlot && item != null){
+      IceRunnable(item, slot, player).runTaskLater(Bukkit.getPluginManager().getPlugin("smItems") as Main, 1)
     }
   }
 

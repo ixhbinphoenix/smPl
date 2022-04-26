136ad5b347475213099ed0adf3cf18f592a69b83
ixhbinphoenix <47122082+ixhbinphoenix@users.noreply.github.com>
2022-04-26 19:59:16 +0200

    feat(smCore): Added Mana

---
+++ smCore/src/main/kotlin/me/ixhbinphoenix/smPl/smCore/events/PlayerSecond.kt
@@ -0,0 +1,38 @@
+package me.ixhbinphoenix.smPl.smCore.events
+
+import me.ixhbinphoenix.smPl.smCore.player.PlayerHandler
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.NamespacedKey
+import org.bukkit.attribute.Attribute
+import org.bukkit.entity.Player
+import org.bukkit.persistence.PersistentDataType
+import org.bukkit.scheduler.BukkitRunnable
+import kotlin.math.roundToInt
+
+class PlayerSecond(val player: Player) : BukkitRunnable() {
+  override fun run() {
+    val handler = PlayerHandler(player)
+
+    if (handler.getMana() > handler.getCurrentMana()) {
+      var diff = handler.getMana() - handler.getCurrentMana()
+      // TODO: Replace this
+      if (diff > 20) diff = 20
+      handler.setCurrentMana(handler.getCurrentMana() + diff)
+    }
+    val vanillaMaxHealth = player.getAttribute(Attribute.GENERIC_MAX_HEALTH)?.value?.roundToInt() ?: 20
+    var maxHealth = player.persistentDataContainer.getOrDefault(
+      NamespacedKey.fromString("smcore:player.maxhealth.int")!!,
+      PersistentDataType.INTEGER,
+      vanillaMaxHealth)
+    if (maxHealth < 20) maxHealth = 20
+    player.sendActionBar(
+      Component.text("${(player.health * (maxHealth / 20)).roundToInt()}/$maxHealth ♥").color(NamedTextColor.RED)
+        .append(Component.text(" ${handler.getCurrentMana()}/${handler.getMana()}").color(NamedTextColor.AQUA)
+          .append(Component.text(" ⚡").color(NamedTextColor.GOLD))
+        )
+        .append(Component.text(" ${handler.getDefence()} ⛨").color(NamedTextColor.GREEN))
+    )
+  }
+
+}
